a09aab88e99ee088ad6c542d41c106551b667cbf
pedrocontreras <fpedro23@gmail.com>
2016-04-13 00:00:58 -0400

    Conf

--- Projects/settings.py
+++ Projects/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'Projects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
