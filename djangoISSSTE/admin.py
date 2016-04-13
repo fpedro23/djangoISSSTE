1f52c886167f7d2c72768a18946744165221fd23
pedrocontreras <fpedro23@gmail.com>
2016-04-12 22:52:47 -0400

    Modelo Inicial

--- djangoISSSTE/admin.py
+++ djangoISSSTE/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from djangoISSSTE.models import *
 # Register your models here.
+
+
+class MetaInline(admin.TabularInline):
+    model = Meta
+
+
+class AccionEstrategicaModelAdmin(admin.ModelAdmin):
+    model = AccionEstrategica
+    inlines = [MetaInline]
+
+
+admin.site.register(UserProfile)
+admin.site.register(Delegacion)
+admin.site.register(Estado)
+admin.site.register(Municipio)
+admin.site.register(Carencia)
+admin.site.register(SubCarencia)
+admin.site.register(AccionEstrategica, AccionEstrategicaModelAdmin)
+admin.site.register(Meta)
