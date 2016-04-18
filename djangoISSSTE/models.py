e73c281201055368da194259ef4d60e00aa103aa
Oscar Sanchez <oscar.sanjim@gmail.com>
2016-04-17 22:10:48 -0500

    Se cambió el modelo. Ahora se basa en el diseño establecido (Queda pendiente el avance por Estado)

--- djangoISSSTE/models.py
+++ djangoISSSTE/models.py
@@ -1,71 +1,12 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
 from django.forms import model_to_dict
 
 
-class Delegacion(models.Model):
-    nombreDelegacion = models.CharField(max_length=200)
-    imagenDelegacion = models.FileField(blank=True, null=True)
-    dependienteDe = models.ForeignKey('self', null=True, blank=True)
-    fecha_ultima_modificacion = models.DateTimeField(null=True, blank=True)
-    orden_secretaria = models.FloatField(null=True, blank=True)
-
-    def __str__(self):  # __unicode__ on Python 2
-        return self.nombreDelegacion
-
-    # def get_contactos(self):
-    #     return Usuario.objects.filter(Q(dependencia=self) & Q(rol='AD'))
-
-    def to_serializable_dict(self):
-        ans = {}
-
-        ans['id'] = str(self.id)
-        ans['nombreDependencia'] = str(self.nombreDelegacion)
-        if self.imagenDelegacion is None or self.imagenDelegacion.name == '' or self.imagenDelegacion.name is None:
-            ans['imagenDependencia'] = None
-        else:
-            ans['imagenDependencia'] = self.imagenDelegacion.url
-
-        if self.dependienteDe is None:
-            ans['dependienteDe'] = None
-        else:
-            ans['dependienteDe'] = str(self.dependienteDe.id)
-
-        if self.fecha_ultima_modificacion is None:
-            ans['fecha_ultima_modificacion'] = None
-        else:
-            ans['fecha_ultima_modificacion'] = self.fecha_ultima_modificacion.__str__()
-
-        return ans
-
-    def get_tree(self):
-        ans = {'dependencia': self.to_serializable_dict(), 'subdependencias': None}
-        subdeps = Delegacion.objects.filter(dependienteDe__id=self.id)
-
-        if subdeps and subdeps.count() > 0:
-            ans['subdependencias'] = []
-            for subdep in subdeps:
-                ans['subdependencias'].append(subdep.get_tree())
-
-        return ans
-
-    def get_subdeps_flat(self):
-        ans = None
-        subdeps = Delegacion.objects.filter(dependienteDe__id=self.id)
-
-        if subdeps and subdeps.count() > 0:
-            ans = []
-            for subdep in subdeps:
-                subsubdeps = subdep.get_subdeps_flat()
-                if subsubdeps:
-                    ans.append(subsubdeps)
-
-        return ans
-
-
-@python_2_unicode_compatible
 class Estado(models.Model):
     nombreEstado = models.CharField(max_length=200)
     latitud = models.FloatField()
@@ -74,21 +15,18 @@ class Estado(models.Model):
     def __str__(self):  # __unicode__ on Python 2
         return self.nombreEstado
 
-    def __unicode__(self):  # __unicode__ on Python 2
-        return self.nombreEstado
-
     def to_serializable_dict(self):
         ans = model_to_dict(self)
         ans['id'] = str(self.id)
         return ans
 
 
-@python_2_unicode_compatible
-class Municipio(models.Model):
+
+class Municipio (models.Model):
     nombreMunicipio = models.CharField(max_length=200)
     latitud = models.FloatField()
     longitud = models.FloatField()
-    estado = models.ForeignKey(Estado, null=False, blank=False)
+    estado = models.ForeignKey(Estado, null=False, blank=False, default="")
 
     def to_serializable_dict(self):
         ans = model_to_dict(self)
@@ -99,80 +37,129 @@ class Municipio(models.Model):
     def __str__(self):
         return self.nombreMunicipio
 
-    def __unicode__(self):
-        return self.nombreMunicipio
 
-
+@python_2_unicode_compatible
 class Carencia(models.Model):
-    nombreCarencia = models.CharField(max_length=200)
+    nombreCarencia = models.CharField(max_length=200, null=False, blank=False)
 
-    def __str__(self):
+    def __str__(self):      # __unicode__ on Python 2
         return self.nombreCarencia
 
-    def __unicode__(self):
-        return self.nombreCarencia
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        return ans
 
 
+@python_2_unicode_compatible
 class SubCarencia(models.Model):
-    carencia = models.ForeignKey(Carencia)
-    nombreSubcarencia = models.CharField(max_length=200)
+    nombreSubCarencia = models.CharField(max_length=200)
+    carencia = models.ForeignKey(Carencia, null=False, blank=False);
 
     def __str__(self):
-        return self.nombreSubcarencia
+        return self.nombreSubCarencia
 
-    def __unicode__(self):
-        return self.nombreSubcarencia
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        ans['carencia'] = self.carencia.nombreCarencia
+        return ans
 
 
+@python_2_unicode_compatible
+class Responsable(models.Model):
+    nombreResponsable = models.CharField(max_length=100)
+    cargoResponsable = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombreResponsable
+
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        return ans
+
+
+@python_2_unicode_compatible
 class AccionEstrategica(models.Model):
-    carencia = models.ForeignKey(Carencia)
-    delegacion = models.ForeignKey(Delegacion)
-    subcarencia = models.ForeignKey(SubCarencia)
-    nombreAccionEstrategica = models.CharField(max_length=100)
-    unidadDeMedida = models.CharField(max_length=200)
-    descripcionAccionEstrategica = models.TextField(max_length=300)
+    nombreAccion = models.CharField(max_length=200, null=False, blank=False)
+    unidadDeMedida = models.CharField(max_length=200, null=False, blank=False)
+    subCarencia = models.ForeignKey(SubCarencia, null=False, blank=False)
+    responsable = models.ForeignKey(Responsable)
 
     def __str__(self):
-        return self.nombreAccionEstrategica
+        return self.nombreAccion
 
-    def __unicode__(self):
-        return self.nombreAccionEstrategica
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        ans['subCarencia'] = str(self.subCarencia.nombreSubCarencia)
+        ans['responsable'] = str(self.responsable.nombreResponsable)
+        return ans
 
 
 class Meta(models.Model):
-    accionEstrategica = models.ForeignKey(AccionEstrategica)
-    entidad = models.ForeignKey(Estado)
-    municipio = models.ForeignKey(Municipio)
-    mes = models.CharField(max_length=200)
-    cantidad = models.FloatField()
-
-    def __str__(self):
-        return self.accionEstrategica.nombreAccionEstrategica
-
-    def __unicode__(self):
-        return self.accionEstrategica.nombreAccionEstrategica
-
-
-# class AvanceMensual(models.Model):
-#     mes = models.CharField(max_length=200)
-#     unidad = models.FloatField()
-#     meta = models.ForeignKey(Meta)
-#
-#     def __str__(self):
-#         return self.mes
-#
-#     def __unicode__(self):
-#         return self.mes
+    accionEstrategica = models.ForeignKey(AccionEstrategica, null=False, blank=False)
+    estado = models.ForeignKey(Estado, null=False, blank=False)
+    periodo = models.IntegerField()
+    enero = models.FloatField()
+    febrero = models.FloatField()
+    marzo = models.FloatField()
+    abril = models.FloatField()
+    mayo = models.FloatField()
+    junio = models.FloatField()
+    julio = models.FloatField()
+    agosto = models.FloatField()
+    septiembre = models.FloatField()
+    octubre = models.FloatField()
+    noviembre = models.FloatField()
+    diciembre = models.FloatField()
+    inversionAprox = models.FloatField()
 
 
-class UserProfile(models.Model):
-    ADMIN = 'AD'
-    USER = 'US'
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        ans['accionEstrategica'] = self.accionEstrategica.nombreAccion
+        ans['estado'] = self.estado.nombreEstado
+        return ans
+
+
+class AvancePorMunicipio(models.Model):
+    meta = models.ForeignKey(Meta, null=False, blank=False)
+    municipio = models.ForeignKey(Municipio, null=False, blank=False)
+    periodo = models.IntegerField()
+    enero = models.FloatField()
+    febrero = models.FloatField()
+    marzo = models.FloatField()
+    abril = models.FloatField()
+    mayo = models.FloatField()
+    junio = models.FloatField()
+    julio = models.FloatField()
+    agosto = models.FloatField()
+    septiembre = models.FloatField()
+    octubre = models.FloatField()
+    noviembre = models.FloatField()
+    diciembre = models.FloatField()
+    inversionAprox = models.FloatField()
+
+    def to_serialize_dict(self):
+        ans = model_to_dict(self)
+        ans['id'] = str(self.id)
+        ans['meta'] = "meta_ISSTE"
+        ans['municipio'] = self.municipio.nombreMunicipio
+        return ans
+
+
+class Usuario(models.Model):
+    REGIONAL = "RE"
+    ESTATAL = "ES"
 
     ROLES_CHOICES = (
-        (ADMIN, 'Administrador general'),
-        (USER, 'Usuario de Dependencia'),
+        (REGIONAL, 'Administrador Regional'),
+        (ESTATAL, 'Administrador Estatal'),
     )
-    rol = models.CharField(max_length=2, choices=ROLES_CHOICES, default=USER)
+
     user = models.OneToOneField(User)
-    delegacion = models.ForeignKey(Delegacion, blank=True, null=True, )
+    rol = models.CharField(max_length=2, choices=ROLES_CHOICES, default=User)
+    estado = models.ForeignKey(Estado, null=False, blank=False)
