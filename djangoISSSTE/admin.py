396f84076a7ff93263c85de6ae91969a91f86f76
Oscar Sanchez <oscar.sanjim@gmail.com>
2016-04-17 22:43:01 -0500

    Cambio en admin.py para inclui el nuevo modelo

--- djangoISSSTE/admin.py
+++ djangoISSSTE/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
-from djangoISSSTE.models import *
+
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from ISSSTE_Oscar.models import *
+
+class UsuarioInLine(admin.StackedInline):
+	model = Usuario
+	can_delete = False
+	verbose_name_plural = 'Usuario'
+
+class UserAdmin(UserAdmin):
+    inlines = [UsuarioInLine]
 
 
-class MetaInline(admin.TabularInline):
-    model = Meta
 
-
-class AccionEstrategicaModelAdmin(admin.ModelAdmin):
-    model = AccionEstrategica
-    inlines = [MetaInline]
-
-
-admin.site.register(UserProfile)
-admin.site.register(Delegacion)
-admin.site.register(Estado)
-admin.site.register(Municipio)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Carencia)
 admin.site.register(SubCarencia)
-admin.site.register(AccionEstrategica, AccionEstrategicaModelAdmin)
+admin.site.register(AccionEstrategica)
 admin.site.register(Meta)
+admin.site.register(AvancePorMunicipio)
+admin.site.register(Responsable)
+admin.site.register(Estado)
+admin.site.register(Municipio)
