455ef5a75ad349a23bfebc7bcb34c5aa1feabdf7
pedrocontreras <fpedro23@gmail.com>
2016-04-18 00:13:44 -0400

    Cambio de nombre incorrecto de modulo

--- djangoISSSTE/admin.py
+++ djangoISSSTE/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 
-from ISSSTE_Oscar.models import *
+from djangoISSSTE.models import *
 
 class UsuarioInLine(admin.StackedInline):
 	model = Usuario
