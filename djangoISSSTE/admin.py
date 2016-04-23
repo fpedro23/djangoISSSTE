6d227bbd9dd51232d8f616a8e7e16e2d82e0c23c
Oscar Sanchez <oscar.sanjim@gmail.com>
2016-04-23 17:05:35 -0500

    Views de usuario listas

--- djangoISSSTE/admin.py
+++ djangoISSSTE/admin.py
@@ -1,62 +1,15 @@
 # coding=utf-8
 from django import forms
 from django.contrib import admin
+#from django.contrib.admin.models import LogEntry
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
+from django.db.models.query_utils import Q
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import Group
 from djangoISSSTE.models import *
 
-
-class UsuarioInLine(admin.StackedInline):
-	model = Usuario
-	verbose_name_plural = 'Usuario'
-
-
-class UserAdmin(UserAdmin):
-	inlines = (UsuarioInLine,)
-	list_display = ('username', 'first_name', 'last_name', 'email',)
-
-	'''
-	def response_add(self, request, obj, post_url_continue=None):
-		if '_addanother' not in request.POST:
-			return HttpResponseRedirect('/admin/auth/user')
-		else:
-			return super(UserAdmin, self).response_add(request, obj, post_url_continue)
-	'''
-
-	def save_model(self, request, obj, form, change):
-		obj.is_staff = True
-		usuario = obj
-		usuario.save()
-
-		if usuario.usuario.rol == "AG":
-			g = Group.objects.get(name = "administrador_general")
-			g.user_set.add(usuario)
-			print "Definiendo permisos para Administrador General"
-
-		elif usuario.usuario.rol == "UC":
-			g = Group.objects.get(name="usuario")
-			g.user_set.add(usuario)
-			print "Definiendo permisos para Usuario Central"
-
-		elif usuario.usuario.rol == "FC":
-			g = Group.objects.get(name="funcionario")
-			g.user_set.add(usuario)
-			print "Definiendo permisos para Funcionario Central"
-
-		elif usuario.usuario.rol == "UE":
-			g = Group.objects.get(name="usuario")
-			g.user_set.add(usuario)
-			print "Definiendo permisos para Usuario Estatal"
-
-		elif usuario.usuario.rol == "FE":
-			g = Group.objects.get(name="funcionario")
-			g.user_set.add(usuario)
-			print "Definiendo permisos para Funcionario Estatal"
-
-		super(UserAdmin, self).save_model(request, obj, form, change)
 
 
 class MetaMensualInLine(admin.TabularInline):
@@ -103,6 +56,77 @@ class AccionEstrategicaAdmin(admin.ModelAdmin):
 	get_carencia.short_description = 'Carencia'
 
 
+class UsuarioInLine(admin.StackedInline):
+	model = Usuario
+	verbose_name_plural = 'Usuario'
+
+	can_delete = False
+	verbose_name_plural = 'Usuario'
+	extra = 1
+
+
+class UserAdmin(UserAdmin):
+	inlines = (UsuarioInLine,)
+	list_display = ('username', 'first_name', 'last_name', 'email','get_estado')
+	fieldsets = (
+		(('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+		(('AuthInfo'), {'fields': ('username', 'password')}),
+		(('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+		(('Important dates'), {'fields': ('last_login', 'date_joined')}),
+	)
+	add_fieldsets = (
+		(('AuthInfo'), {'fields': ('username', 'password1', 'password2')}),
+		(('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+		(('Permissions'), {'fields': ('is_active',)}),
+	)
+
+	def response_add(self, request, obj, post_url_continue=None):
+		if '_addanother' not in request.POST:
+			return HttpResponseRedirect('/admin/auth/user')
+		else:
+			return super(UserAdmin, self).response_add(request, obj, post_url_continue)
+
+	def save_model(self, request, obj, form, change):
+		obj.is_staff = True
+		usuario = obj
+		usuario.save()
+
+		if usuario.usuario.rol == "AG":
+			g = Group.objects.get(name="administrador_general")
+			g.user_set.add(usuario)
+			print "Definiendo permisos para Administrador General"
+
+		elif usuario.usuario.rol == "UC":
+			g = Group.objects.get(name="usuario")
+			g.user_set.add(usuario)
+			print "Definiendo permisos para Usuario Central"
+
+		elif usuario.usuario.rol == "FC":
+			g = Group.objects.get(name="funcionario")
+			g.user_set.add(usuario)
+			print "Definiendo permisos para Funcionario Central"
+
+		elif usuario.usuario.rol == "UE":
+			g = Group.objects.get(name="usuario")
+			g.user_set.add(usuario)
+			print "Definiendo permisos para Usuario Estatal"
+
+		elif usuario.usuario.rol == "FE":
+			g = Group.objects.get(name="funcionario")
+			g.user_set.add(usuario)
+			print "Definiendo permisos para Funcionario Estatal"
+
+		super(UserAdmin, self).save_model(request, obj, form, change)
+
+	def delete_model(self, request, obj):
+		self.message_user(request, "Usuario eliminado satisfactoriamente", )
+		super(UserAdmin, self).delete_model(request, obj)
+
+	def get_estado(self, obj):
+		return obj.usuario.estado
+
+
+#admin.site.register(LogEntry)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Carencia)
