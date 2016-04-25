bc784bf02443c7744d29de0a6bab634dd5d78c11
Oscar Sanchez <oscar.sanjim@gmail.com>
2016-04-24 22:07:14 -0500

    Se implemento API para estados y municipios por estado

--- djangoISSSTE/api.py
+++ djangoISSSTE/api.py
@@ -8,9 +8,39 @@ from oauth2_provider.views.generic import ProtectedResourceView
 from djangoISSSTE.models import *
 
 
+def get_array_or_none(the_string):
+	if the_string is None:
+		return None
+	else:
+		return map(int, the_string.split(','))
+
 
 class EstadosEndpoint(ProtectedResourceView):
 	def get(self, request):
 		return HttpResponse(json.dumps(map(lambda estado: estado.to_serializable_dict(), Estado.objects.all())),
 							'application/json')
 
+
+class MunicipiosForEstadosEndpoint(ProtectedResourceView):
+	def get(self, request):
+		estado_ids = get_array_or_none(request.GET.get('estados'))
+		all_estados = False
+
+		if estado_ids is None:
+			all_estados = True
+		else:
+			for estado_id in estado_ids:
+				if estado_id == 33 or estado_id == 34:
+					all_estados = True
+					break
+
+		if all_estados:
+			municipios = Municipio.objects.order_by('nombreMunicipio').all()
+		else:
+			municipios = Municipio.objects.filter(estado_id__in=estado_ids).order_by('nombreMunicipio').all()
+
+		the_list = []
+		for municipio in municipios.values('id', 'nombreMunicipio'):
+				the_list.append(municipio)
+
+		return HttpResponse(json.dumps(the_list), 'application/json')
