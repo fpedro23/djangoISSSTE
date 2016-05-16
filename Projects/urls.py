e520168a8867940e4c623ce726fda1a76c2e02a8
Oscar Sanchez <oscar.sanjim@gmail.com>
2016-05-15 21:33:41 -0500

    En admin meta, se concatenó: carencia-accion-unidad

--- Projects/urls.py
+++ Projects/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     url(r'^usuarios', djangoISSSTE.views.usuarios, name='usuarios'),
     url(r'^movimientos', djangoISSSTE.views.movimientos, name='movimientos'),
 
-     url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
     url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
