73045d9857146276c264c225eaf90f754385d29c
agsneutron <ags_neutron@hotmail.com>
2017-01-26 10:48:43 -0600

    configuración para el issste

--- Projects/wsgi.py
+++ Projects/wsgi.py
@@ -6,6 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+
+#!/usr/bin/env python
+
 import os
 import sys
 import site
@@ -14,14 +17,14 @@ os.environ['PYTHON_EGG_CACHE'] = '/eggCache'
 
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('/var/www/html/isssteEnv/lib/python2.7/site-packages')
+site.addsitedir('/usr/local/apache24/htdocs/inclusioni/isssteEnv/lib/python2.7/site-packages')
 
 # Add the app's directory to the PYTHONPATH
-sys.path.append('/var/www/html/djangoISSSTE')
-sys.path.append('/var/www/html/djangoISSSTE/djangoISSSTE')
+sys.path.append('/usr/local/apache24/htdocs/inclusioni/djangoISSSTE')
+sys.path.append('/usr/local/apache24/htdocs/inclusioni/djangoISSSTE/djangoISSSTE')
 
 # Activate your virtual env
-activate_env = os.path.expanduser("/var/www/html/isssteEnv/bin/activate_this.py")
+activate_env = os.path.expanduser("/usr/local/apache24/htdocs/inclusioni/isssteEnv/bin/activate_this.py")
 execfile(activate_env, dict(__file__=activate_env))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Projects.settings")
